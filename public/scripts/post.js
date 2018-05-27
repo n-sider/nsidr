3f1196e66eb56dea70512227c534ee8a8e20b7a7
Cory Faller <Tathanen@users.noreply.github.com>
2018-05-27 13:16:37 -0500

    Scroll to top button

--- public/scripts/post.js
+++ public/scripts/post.js
@@ -4,3 +4,7 @@ if (scrollTarget !== null && scrollTarget - 10 > 0) {
   window.scroll({ top: scrollTarget - 10, behavior: 'smooth' });
 }
 sessionStorage.removeItem('scrollFeature');
+
+document.querySelector('.post-footer .top-icon').addEventListener('click', () => {
+  window.scroll({ top: 0, behavior: 'instant' });
+});
