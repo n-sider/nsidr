bd4521df45f06f0c90cbc0352209f161d1891057
Cory Faller <Tathanen@users.noreply.github.com>
2018-06-04 00:24:13 -0500

    Production bugs

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,9 @@
-document.querySelector('.continue-reading a').addEventListener('click', (e) => {
-  e.preventDefault();
-  sessionStorage.setItem('scrollFeature', window.pageYOffset);
-  window.location.href = document.querySelector('.continue-reading a');
-});
+const continueTarget = document.querySelector('.continue-reading a');
+
+if (continueTarget) {
+  continueTarget.addEventListener('click', (e) => {
+    e.preventDefault();
+    sessionStorage.setItem('scrollFeature', window.pageYOffset);
+    window.location.href = document.querySelector('.continue-reading a');
+  });
+}
