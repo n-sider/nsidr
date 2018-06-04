e3444a82cb758cba2252408d52413ec1e586974b
Cory Faller <Tathanen@users.noreply.github.com>
2018-06-03 22:51:07 -0500

    Adjustments for in-progress posts

--- templates/helpers.js
+++ templates/helpers.js
@@ -1,6 +1,9 @@
 module.exports = {
   getMood: (user, mood) => {
-    const { filename } = user[`avatar${(mood !== 'Default' && mood) || ''}`];
-    return `https://${process.env.S3FILE_BUCKET}/${filename}`;
+    if (user) {
+      const { filename } = user[`avatar${(mood !== 'Default' && mood) || ''}`];
+      return `https://${process.env.S3FILE_BUCKET}/${filename}`;
+    }
+    return '';
   }
 };
