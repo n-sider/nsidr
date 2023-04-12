d4b775c7aa023760e8088e310c6ed3348e41bb80
Cory Faller <Tathanen@users.noreply.github.com>
2023-04-12 17:09:34 -0500

    Beginnings of Hugo replatforming

--- templates/helpers.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  getMood: (user, mood) => {
-    if (user) {
-      const { filename } = user[`avatar${(mood !== 'Default' && mood) || ''}`];
-      return `https://${process.env.S3FILE_BUCKET}/${filename}`;
-    }
-    return '';
-  }
-};
